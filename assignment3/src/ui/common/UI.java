1611fe8470f28ba1644ccdf74ddff9431163c0b2
zackc0724 <chen.z15@northeastern.edu>
2025-10-12 21:09:38 -0400

    UI update

--- assignment3/src/ui/common/UI.java
+++ assignment3/src/ui/common/UI.java
@@ -9,9 +9,14 @@ public final class UI {
     private UI() {}
     public static void addLogoutBar(JPanel host, JPanel cardPanel, EcoSystem system) {
         JPanel south = new JPanel(new BorderLayout());
+        JLabel status = new JLabel("Ready");
         JButton btnLogout = new JButton("Logout");
+
+        south.add(status, BorderLayout.WEST);
         south.add(btnLogout, BorderLayout.EAST);
         host.add(south, BorderLayout.SOUTH);
+
+        btnLogout.setMnemonic('L'); // Alt+L
         btnLogout.addActionListener(e -> {
             cardPanel.removeAll();
             cardPanel.revalidate();
@@ -20,3 +25,5 @@ public final class UI {
         });
     }
 }
+        
+
